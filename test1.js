be380fcad41511924f2833327ac488eb38f29813
Haochen Li <lihaochen2017@outlook.com>
2017-10-31 20:50:52 -0500

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,31 @@
+var matrix = [
+  [1,2,3,4,5],
+  [2,3,4,5,6],
+  [3,4,5,6,7],
+]
+
+function sumMatrix(matrix) {
+  let sum = 0 ;
+  for(let i = 0; i < matrix.length; i++){
+    for(let j = 0 ; j < matrix[i].length ; j++){
+      sum+=matrix[i][j];
+    }
+  }
+  return sum;
+}
+
+function sumMatrix2(matrix) {
+  let sum = 0;
+  matrix.map((array)=>{
+    array.map((value)=>{
+      sum+=value;
+      return;
+    })
+    return;
+  })
+  return sum;
+}
+
+console.log(sumMatrix(matrix));
+console.log(sumMatrix2(matrix));
+
